8675640d01cbb11e0eaa512d6d822c0fe56b3292
Vaibhav Mahendrasing Rajput <157829577+vaibhavrajput07@users.noreply.github.com>
2025-07-08 02:39:37 +0530

    useLocation add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 
 export default function Login({ onLogin }) {
